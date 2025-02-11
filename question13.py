ac73e5d385e38fdfe9a93da6b69fca60e4047788
shubhan <shubhandua@gmail.com>
2025-02-11 15:09:05 +0530

    day2

---
+++ question13.py
@@ -0,0 +1,11 @@
+def trap(height):
+    if not height:
+        return 0
+
+    left, right = 0, len(height) - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left
